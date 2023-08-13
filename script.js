6b7f145d366933868527269f709e345878884212
RAZRO <razie.robatjazy>
2023-08-14 00:44:05 +0330

    add chance part

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const keyboardBtn = document.querySelectorAll("#keyboard-button");
 const header = document.getElementById("header");
+const gallowsFloor = document.querySelector(".gallows-floor");
+const gallowsWall = document.querySelector(".gallows-wall");
+const gallowsTopFirst = document.querySelector(".gallows-top-first");
+const gallowsTopSecond = document.querySelector(".gallows-top-second");
+const gallowsRope = document.querySelector(".gallows-rope");
+const playerHead = document.querySelector(".player-head");
+const playerBody = document.querySelector(".player-body");
+const playerRightHand = document.querySelector(".player-right-hand");
+const playerLeftHand = document.querySelector(".player-left-hand");
+const playerRightLeg = document.querySelector(".player-right-leg");
+const playerLeftLeg = document.querySelector(".player-left-leg");
+
+
+
 
 
 const words = ["html", "css", "console", "desktop", "computer", "laptop", "phone"];
 var wordString = [];
+var chance = 10;
+let fill = new Boolean(false);
 
 
 // console.log(keyboardBtn);
@@ -13,9 +29,49 @@ var span = [];
 
 var wordIndex = randomNumber(0, 6)
 var word = words[wordIndex];
-console.log(word);
-console.log(word.length);
-console.log(wordIndex);
+// console.log(word);
+// console.log(word.length);
+// console.log(wordIndex);
+
+function ponishment() {
+    switch (Number(chance)) {
+        case 9:
+            gallowsFloor.classList.remove("hiden-body");
+            break;
+        case 8:
+            gallowsWall.classList.remove("hiden-body");
+            break;
+        case 7:
+            gallowsTopFirst.classList.remove("hiden-body");
+            gallowsTopSecond.classList.remove("hiden-body");
+            break;
+        case 6:
+            gallowsRope.classList.remove("hiden-body");
+            break;
+        case 5:
+            playerHead.classList.remove("hiden-body");
+            break;
+        case 4:
+            playerBody.classList.remove("hiden-body");
+            break;
+        case 3:
+            playerRightHand.classList.remove("hiden-body");
+            break;
+        case 2:
+            playerLeftHand.classList.remove("hiden-body");
+            break;
+        case 1:
+            playerRightLeg.classList.remove("hiden-body");
+            break;
+        case 0:
+            playerLeftLeg.classList.remove("hiden-body");
+            break;
+
+
+        default:
+            break;
+    }
+}
 
 for (let index = 0; index < word.length; index++) {
 
@@ -28,11 +84,11 @@ for (let index = 0; index < word.length; index++) {
 for (let index = 0; index < word.length; index++) {
     wordString[index] = word.charAt(index);
 }
-console.log(wordString);
-console.log(wordString[0]);
+// console.log(wordString);
+// console.log(wordString[0]);
 function fillSpan(index) {
     for (let index2 = 0; index2 < word.length; index2++) {
-        console.log(keyboardBtn[index].textContent);
+        // console.log(keyboardBtn[index].textContent);
 
         if (wordString[index2] == keyboardBtn[index].textContent.toLowerCase()) {
             span[index2].textContent = keyboardBtn[index].textContent;
@@ -40,14 +96,27 @@ function fillSpan(index) {
             keyboardBtn[index].classList.remove("from-white");
             keyboardBtn[index].classList.remove("to-zinc-200");
             keyboardBtn[index].classList.add("color-accept");
+            fill = true;
         } else {
-            keyboardBtn[index].disabled = true;
-            keyboardBtn[index].classList.remove("from-white");
-            keyboardBtn[index].classList.remove("to-zinc-200");
-            keyboardBtn[index].classList.remove("bg-gradient-to-b");
-            keyboardBtn[index].classList.add("bg-gray-400");
+            if (index2 == word.length - 1 && !fill) {
+                keyboardBtn[index].disabled = true;
+                keyboardBtn[index].classList.remove("from-white");
+                keyboardBtn[index].classList.remove("to-zinc-200");
+                keyboardBtn[index].classList.remove("bg-gradient-to-b");
+                keyboardBtn[index].classList.add("bg-gray-400");
+                chance = chance - 1;
+                console.log(`your chance is: ${chance}`);
+                ponishment();
+                setTimeout(function() {
+                    if(Number(chance)==0){
+                        window.alert("you loose!!!!!!")
+                    }                 
+                 }, 2000);
+            }
+
 
         }
+
 
     }
     // fill = false;
@@ -75,6 +144,7 @@ function randomNumber(min, max) {
 for (let index = 0; index < 26; index++) {
 
     keyboardBtn[index].addEventListener('click', () => {
+        fill = false;
         fillSpan(index);
     });
 }
